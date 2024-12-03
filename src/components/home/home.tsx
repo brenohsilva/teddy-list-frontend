7b6aa21a3c02dc48f70d18b2ff265ea802bb1b45
ed-breno <breno.henrique@empreenderdinheiro.com>
2024-12-03 20:53:35 -0300

    revert merge

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,35 +1,14 @@
 // src/components/Home.tsx
-import React, { useState } from "react";
-import styles from "./Home.module.css";
-
+import ClientList from "../clientList/clientList";
+import { useUser } from "../context/UserContext";
+import Header from "../header/header";
 const Home: React.FC = () => {
-  const [name, setName] = useState<string>("");
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-
-  const handleButtonClick = () => {
-    if (name.trim()) {
-      alert(`Bem-vindo, ${name}!`);
-    } else {
-      alert("Por favor, insira seu nome.");
-    }
-  };
+  const { username } = useUser();
 
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <h1 className="mb-3 fs-4">Olá, seja bem-vindo!</h1>
-      <input
-        type="text"
-        placeholder="Digite seu nome"
-        value={name}
-        onChange={handleInputChange}
-        className={styles.input}
-      />
-      <button onClick={handleButtonClick} className={styles.button}>
-        Entrar
-      </button>
+    <div>
+      <Header username={username} ></Header>
+      <ClientList></ClientList>
     </div>
   );
 };
