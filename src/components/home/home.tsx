1e6ba79b88b45a91362da0057d1f46b9148d9d31
ed-breno <breno.henrique@empreenderdinheiro.com>
2024-12-02 23:20:56 -0300

    :tada: first commit

---
+++ src/components/home/home.tsx
@@ -0,0 +1,37 @@
+// src/components/Home.tsx
+import React, { useState } from "react";
+import styles from "./Home.module.css";
+
+const Home: React.FC = () => {
+  const [name, setName] = useState<string>("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    if (name.trim()) {
+      alert(`Bem-vindo, ${name}!`);
+    } else {
+      alert("Por favor, insira seu nome.");
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Olá, seja bem-vindo!</h1>
+      <input
+        type="text"
+        placeholder="Digite seu nome"
+        value={name}
+        onChange={handleInputChange}
+        className={styles.input}
+      />
+      <button onClick={handleButtonClick} className={styles.button}>
+        Entrar
+      </button>
+    </div>
+  );
+};
+
+export default Home;
