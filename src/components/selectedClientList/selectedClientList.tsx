66bfb8dbb2fe8edf4b4402795446df8b5c408ad0
ed-breno <breno.henrique@empreenderdinheiro.com>
2024-12-04 12:59:54 -0300

    :recycle: add empty message if there were no client selected

--- src/components/selectedClientList/selectedClientList.tsx
+++ src/components/selectedClientList/selectedClientList.tsx
@@ -9,10 +9,11 @@ import { Client } from "../../api/clientService";
 const SelectedClientList: React.FC = () => {
   const { username } = useUser();
   const { selectedClients, removeClient, clearClients } = useSelectedClients();
+
   const handleSelectClient = (client: Client) => {
     if (selectedClients.find((c) => c.id === client.id)) {
       removeClient(client.id);
-    } 
+    }
   };
 
   return (
@@ -20,27 +21,37 @@ const SelectedClientList: React.FC = () => {
       <Header username={username}></Header>
       <div className="clientList container mt-5">
         <h5 className="mb-4 fw-normal">Clientes selecionados:</h5>
-        <div className="row">
-          {selectedClients.map((client) => (
-            <div key={client.id} className="col-md-3">
-              <SelectedClientCard
-                name={client.firstName}
-                salary={client.salary}
-                companyValue={client.companyValue}
-                isSelected={true}
-                onRemove={() => handleSelectClient(client)}
-              />
-            </div>
-          ))}
-        </div>
-        <div className="d-flex justify-content-center w-100 mt-5">
-          <button
-            className={`btn buttonPrimaryColor w-100`}
-            onClick={clearClients}
-          >
-            Limpar clientes selecionados
-          </button>
-        </div>
+
+        {selectedClients.length === 0 ? (
+          <div className="d-flex justify-content-center align-items-center" style={{ height: "50vh" }}>
+            <p className="text-muted fs-5">Nenhum cliente selecionado.</p>
+          </div>
+        ) : (
+          <div className="row">
+            {selectedClients.map((client) => (
+              <div key={client.id} className="col-md-3">
+                <SelectedClientCard
+                  name={client.firstName}
+                  salary={client.salary}
+                  companyValue={client.companyValue}
+                  isSelected={true}
+                  onRemove={() => handleSelectClient(client)}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+
+        {selectedClients.length > 0 && (
+          <div className="d-flex justify-content-center w-100 mt-5">
+            <button
+              className={`btn buttonPrimaryColor w-100`}
+              onClick={clearClients}
+            >
+              Limpar clientes selecionados
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
