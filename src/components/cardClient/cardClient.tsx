6fc3d45265ebaf641bd4143ca940bfdda1707025
ed-breno <breno.henrique@empreenderdinheiro.com>
2024-12-04 08:11:19 -0300

    :art: changing color of the selected itens

--- src/components/cardClient/cardClient.tsx
+++ src/components/cardClient/cardClient.tsx
@@ -20,6 +20,13 @@ const CardClient: React.FC<CardClientProps> = ({
   onEdit,
   onDelete,
 }) => {
+
+  const formatToCurrency = (value: string) => {
+    const numericValue = parseFloat(value); 
+    if (isNaN(numericValue)) return "0,00"; 
+    return numericValue.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+  };
+
   return (
     <div className={`card shadow-sm p-2 pb-0 mb-2 ${
       isSelected ? `${styles.cardSelected} text-white` : ""
@@ -28,17 +35,17 @@ const CardClient: React.FC<CardClientProps> = ({
         
         <h5 className="card-title d-flex justify-content-center fw-bold ">{name}</h5>
         <p className="card-text d-flex justify-content-center gap-2 m-2 p-0 ">
-          <p className="m-0 p-0">Salário:</p> R$ {salary}
+          <p className="m-0 p-0">Salário:</p> {formatToCurrency(salary)}
         </p>
         <p className="card-text d-flex justify-content-center gap-2 ">
-          <p className="m-0 p-0 fw-normal">Empresa:</p> R$ {companyValue}
+          <p className="m-0 p-0 fw-normal">Empresa:</p> {formatToCurrency(companyValue)}
         </p>
         <div className="d-flex justify-content-between gap-2 mb-0 pb-0">
           <button
             className="btn d-flex align-items-center gap-1 "
             onClick={onSelect}
           >
-            <i className="bi bi-plus-lg "></i> 
+             {!isSelected ? <i className="bi bi-plus-lg "></i> : <i className="bi bi-check2-circle"></i>}
           </button>
           <button
             className="btn  d-flex text-black align-items-center gap-1"
